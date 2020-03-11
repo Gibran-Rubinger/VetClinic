2fd591d3a034a3bd27a6a5ce2f7d49650744e0da
Gibran Rubinger <gibranrubinger@gmail.com>
2020-03-11 23:49:12 +0000

    TypeWrite
    using a for loop to print menssage with delay.
    
    Solution by:
    nodehead
    
    https://youtu.be/7DCSDD-UspA

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 
+//above are being import the super classes that it will be used in the code.
 public class Main {
 
+//	this is the constructor that will call the objects (methods)	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Graphical graphicalPrint = new Graphical();
-		graphicalPrint.screen1();
+		graphicalPrint.typeWriter(null, 1);
+		
 	}
 
 }
