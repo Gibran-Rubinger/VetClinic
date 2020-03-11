5e60973b53476affa776d020ac817f471592349b
Gibran Rubinger <gibranrubinger@gmail.com>
2020-03-11 21:15:22 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Graphical graphicalPrint = new Graphical();
+		graphicalPrint.screen1();
+	}
+
+}
