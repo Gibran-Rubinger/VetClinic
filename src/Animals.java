4cf341000c843586df435a7f0e681e938883e78e
Ana-Araujo <61157834+Ana-Araujo@users.noreply.github.com>
2020-03-18 11:43:50 +0000

    Animals class
    Produce the Animal class and the getters and setters  that will define the animals.

---
+++ src/Animals.java
@@ -0,0 +1,65 @@
+
+public class Animals {
+
+//  attributes
+	private String idAnimal = null;
+	private String name = null;
+	private int age = 0;
+	private boolean species = true;
+	private boolean gender = true;
+	private String medicalCond = null;
+
+//	produce the methods for getters and setters.
+	
+	public String getIdAnimal() {
+		return idAnimal;
+	}
+	
+	public void setIdAnimal(String idAnimal) {
+		this.idAnimal = idAnimal;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public boolean isSpecies() {
+		return species;
+	}
+	
+	public void setSpecies(boolean species) {
+		this.species = species;
+	}
+	
+	public boolean isGender() {
+		return gender;
+	}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	
+	public String getMedicalCond() {
+		return medicalCond;
+	}
+	
+	public void setMedicalCond(String medicalCond) {
+		this.medicalCond = medicalCond;
+	}
+	
+	
+	
+
+	
+}
