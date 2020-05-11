3b34b9912847d50d5bd39709bdc899ba44a4f17f
Gibran Rubinger <gibranrubinger@gmail.com>
2020-05-11 23:03:18 +0100

    change random pics
    up

--- src/animals/reptile/Chameleon.java
+++ src/animals/reptile/Chameleon.java
@@ -40,19 +40,19 @@ Random myRandom = new Random();
 	public void info() {
 		// TODO Auto-generated method stub
 		printer.typeWriter(" Hi my name is "+getName()+" I am a "+getGender()+" and I am very happy to meet you today.\n"
-				+"My owner "+owner.getOwnerName()+ " and I having being fun since "+(getAge() - 2020)+" when I born.", 60);	
+				+"My owner "+owner.getOwnerName()+ " and I having being fun since "+(getAge() - 2020)+" when I was born.", 60);	
 			
 				
 		query.YesOrNo(3,"\n\n\n\n Would you like to see my picture now?  \n\n type Yes or No.");
 		
 		if (query.getUserYesOrNoValidOptio() == 1) {
 
-			int imageGenerator = myRandom.nextInt(5);
+			int imageGenerator = myRandom.nextInt(4);
 //			picture 1
 			switch(imageGenerator) {
-			case 1: 
+			case 0: 
 			printer.typeWriter(""
-					+ "<------- ____\r\n" + 
+					+ "<-------____\r\n" + 
 					"         /    \\  __ _____,\r\n" + 
 					"    `-- |  o   \\'  `  &&/\r\n" + 
 					"       `|      | o  },-'\r\n" + 
@@ -65,9 +65,9 @@ Random myRandom = new Random();
 					"                \\_/"
 					+ "      NO PHOTO REGISTER   ", 1);
 				break;
-			case 2:
+			case 1:
 				printer.typeWriter(""
-						+ "-'\"\"\"`-.\r\n" + 
+						+ " -'\"\"\"`-.\r\n" + 
 						" .'  .-.-.  `.\r\n" + 
 						"/ |--| | |--| \\\r\n" + 
 						"| |  `-^-'  | |\r\n" + 
@@ -80,7 +80,7 @@ Random myRandom = new Random();
 						"   (___|___)"
 						+ "NO PHOTO REGISTER", 1);
 				break;
-			case 3:
+			case 2:
 				printer.typeWriter(""
 						+ "\\,`/ / \r\n" + 
 						" _)..  `_\r\n" + 
@@ -90,9 +90,9 @@ Random myRandom = new Random();
 						"   _||_ ~-/  "
 						+ "NO PHOTO REGISTER", 1);
 				break;
-			case 4:
+			case 3:
 				printer.typeWriter(""
-						+ "     .-\"-.\r\n" + 
+						+ "      .-\"-.\r\n" + 
 						"       /  - -\\\r\n" + 
 						"       \\  @ @/\r\n" + 
 						"        (_ <_)\r\n" + 
@@ -103,9 +103,9 @@ Random myRandom = new Random();
 						"     ((__|__))"
 						+ "NO PHOTO REGISTER", 1);
 				break;
-			case 5:
+			case 4:
 				printer.typeWriter(""
-						+ "     _  .   .   .\r\n" + 
+						+ "         _  .   .   .\r\n" + 
 						"         .' '; '-' '-'|-.\r\n" + 
 						"        (     '------.'  )\r\n" + 
 						"         ;            \\ /\r\n" + 
